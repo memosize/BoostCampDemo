51eb51226d43c5eab27b581348c6b63444591fa8
yangsenlin <89068024@qq.com>
2022-02-12 17:25:55 +0800

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 160,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  proseWrap: 'preserve',
+  tabWidth: 4
+}
